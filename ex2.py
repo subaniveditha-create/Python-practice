0b12c7e5d3b34da3516a9501a6f71c01b3a56292
subaniveditha-create <subaniveditha@gmail.com>
2026-09-09 06:56:09 +0530

    Delete ex2.py

--- ex2.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter your name: ")
-
-print("Hello",name)
-job = input("Enter your current job: ")
-goal = input("Enter your dream job:")
-print("---------CAREER Card-----------")
-print("Name:",name)
-print("Current Job:",job)
-print("Dream Job:",goal)
